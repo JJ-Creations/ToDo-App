db8a1adf0ca8e0398576676bf0769a841621526b
Jino Jenz <jenzthomas78@gmail.com>
2025-07-05 09:15:56 +0530

    Changing all

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const { createActivity, getActivity, deleteActivity } = require("./controllers/activityControllers");
 const app = express();
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: "https://todoappjino.netlify.app",
+  methods: ["GET", "POST", "DELETE", "PUT"],
+  credentials: true
+}));
 const PORT = process.env.PORT || 8010;
 app.listen(PORT, () => console.log("Server running on", PORT));
 
